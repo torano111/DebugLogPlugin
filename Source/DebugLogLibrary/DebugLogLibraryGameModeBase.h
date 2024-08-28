c21605106f2f73292743e8f87a92aaab8cd34b8f
torano <fastrivertiger@gmail.com>
2024-08-28 18:48:22 +0900

    initial

---
+++ Source/DebugLogLibrary/DebugLogLibraryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DebugLogLibraryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEBUGLOGLIBRARY_API ADebugLogLibraryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
